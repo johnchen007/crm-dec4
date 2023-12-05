6712d05da926219d4762b56c3dd95bdb7d385135
Gubbala Bala Sudhakar <126678172+codewithbala@users.noreply.github.com>
2023-12-05 11:33:33 -0500

    CRM Project

---
+++ src/test/java/com/snva/crmproject/CrmprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snva.crmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
