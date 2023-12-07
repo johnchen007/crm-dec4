fde499126d9b103348640f7517411aaf9e0e446e
ramyaperam <peram.ramya77@gmail.com>
2023-12-07 17:58:05 -0600

    this is candidate updated code

--- src/main/java/com/snva/crmproject/controller/CandidateController.java
+++ src/main/java/com/snva/crmproject/controller/CandidateController.java
@@ -1,6 +1,5 @@
 package com.snva.crmproject.controller;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,36 +9,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.snva.crmproject.entity.Candidate;
+import com.snva.crmproject.entity.CandidateBasicDetails;
 import com.snva.crmproject.service.CandidateService;
+
 @RestController
-@RequestMapping("/candidates")
+@RequestMapping("/Candidates")
 public class CandidateController {
+
     @Autowired
     private CandidateService candidateService;
 
     @PostMapping("/add")
-    public String addCandidate(@RequestBody Candidate candidate) {
-        return candidateService.addNewCandidate(candidate);
+    public String addCandidate(@RequestBody CandidateBasicDetails candidateBasicDetails) {
+        System.out.println(candidateBasicDetails.getDetails().getAddressCity());
+    	return candidateService.addNewCandidate(candidateBasicDetails);
     }
 
     @GetMapping("/all")
-    public List<Candidate> getAllCandidates() {
+    public List<CandidateBasicDetails> getAllCandidates() {
         return candidateService.getAllCandidates();
     }
 
     @GetMapping("/{candidateId}")
-    public Candidate getCandidateById(@PathVariable Long candidateId) {
+    public CandidateBasicDetails getCandidateById(@PathVariable String candidateId) {
         return candidateService.getCandidateById(candidateId);
     }
 
     @PutMapping("/update")
-    public String updateCandidate(@RequestBody Candidate updatedCandidate,
-                                  @RequestParam String recruiterName,
-                                  @RequestParam String recruitAdmin) {
-        return candidateService.updateCandidate(updatedCandidate, recruiterName, recruitAdmin);
+    public String updateCandidate(@RequestBody CandidateBasicDetails updatedCandidate) {
+        return candidateService.updateCandidate(updatedCandidate);
     }
 }
--- src/main/java/com/snva/crmproject/service/CandidateService.java
+++ src/main/java/com/snva/crmproject/service/CandidateService.java
@@ -1,14 +1,12 @@
 package com.snva.crmproject.service;
 
-
 import java.util.List;
 
-import com.snva.crmproject.entity.Candidate;
+import com.snva.crmproject.entity.CandidateBasicDetails;
 
 public interface CandidateService {
-    String addNewCandidate(Candidate candidate);
-    List<Candidate> getAllCandidates();
-    Candidate getCandidateById(Long candidateId);
-    String updateCandidate(Candidate updatedCandidate, String recruiterName, String recruitAdmin);
-    
+    String addNewCandidate(CandidateBasicDetails candidate);
+    List<CandidateBasicDetails> getAllCandidates();
+    CandidateBasicDetails getCandidateById(String candidateId);
+    String updateCandidate(CandidateBasicDetails updatedCandidate);
 }
